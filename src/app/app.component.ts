773c29c48880cbc1f7303d8c486a36fd2ab0bd81
MKortmann <companymvms@gmail.com>
2024-01-31 21:08:34 +0100

    old syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { ModeratorDashboardComponent } from './components/moderator-dashboard/moderator-dashboard.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { LazyloadingComponent } from './components/lazyloading/lazyloading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AdminDashboardComponent, ModeratorDashboardComponent, UserDashboardComponent, LazyloadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular17';
+  title = 'angular 17';
+
+  users = ["Matthias","Joe","Anna"];
+  loggedIn = true;
+  accessLevel: string = "admin"
+
+
 }
---
+++ src/app/components/lazyloading/lazyloading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lazyloading',
+  standalone: true,
+  imports: [],
+  templateUrl: './lazyloading.component.html',
+  styleUrl: './lazyloading.component.scss'
+})
+export class LazyloadingComponent {
+
+}
